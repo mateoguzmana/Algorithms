53761697964a4b19382672dd182faa8af752f732
Mateo Guzmán <mateo.guzmanalvarez@gmail.com>
2017-08-28 16:45:54 -0500

    Modify structure

--- src/components/AlgorithmsList.jsx
+++ src/components/AlgorithmsList.jsx
@@ -1,20 +1,38 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import AlgorithmsJsonList from '../data/AlgorithmsJsonList';
 
-export default class Header extends Component {
-  render() {
-    return (
-      <div>
-          <table className="table table-hover">
-              <thead>
-                  <th>Name</th>
-              </thead>
-              <tbody>
-                  <tr>
-                      <td>Test</td>
-                  </tr>
-              </tbody>
-          </table>
-      </div>
-    );
-  }
+export default class AlgorithmsList extends Component {
+    render() {
+        return (
+            <div>
+                <table className="table table-hover">
+                    <thead>
+                        <th>Name</th>
+                        <th>Name</th>
+                        <th>Author</th>
+                        <th></th>
+                    </thead>
+                    <tbody>
+                        {
+                            AlgorithmsJsonList.map((data, index) => {
+                                return (
+                                    <tr key={index}>
+                                        <td>{index+1}</td>
+                                        <td>{data.name}</td>
+                                        <td>{data.author}</td>
+                                        <td>
+                                            <button className="btn btn-info">
+                                                View
+                                            </button>
+                                        </td>
+                                    </tr>
+                                )
+                            }
+                            )
+                        }
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
 }
---
+++ src/data/AlgorithmsJsonList.js
@@ -0,0 +1,13 @@
+let AlgorithmsJsonList;
+export default AlgorithmsJsonList = [
+    {
+        name: "Binary Gap",
+        author: "Mateo Guzmán",
+        link: "link"
+    },
+    {
+        name: "Odd Occurrences In Array",
+        author: "Mateo Guzmán",
+        link: "link"
+    }
+]
