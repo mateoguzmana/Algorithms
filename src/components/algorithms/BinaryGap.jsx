e3be8b083424f96bdf141fcb0a1aa16f7b003caf
Mateo Guzmán <mateo.guzmanalvarez@gmail.com>
2017-08-28 16:53:07 -0500

    Added router package

---
+++ src/components/algorithms/BinaryGap.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class BinaryGap extends Component {
+
+    constructor(props) {
+        super(props);
+
+        console.log("Binary Gap");
+    }
+
+    render() {
+        return (
+            <div>Binary Gap</div>
+        );
+    }
+}
