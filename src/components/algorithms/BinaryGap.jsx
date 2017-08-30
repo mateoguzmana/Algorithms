2491b09bd787d3198adc4eb222a45926d9eedd79
Mateo Guzmán <mateo.guzmanalvarez@gmail.com>
2017-08-30 11:51:15 -0500

    Binary gap view creation

--- src/components/algorithms/BinaryGap.jsx
+++ src/components/algorithms/BinaryGap.jsx
@@ -5,12 +5,28 @@ export default class BinaryGap extends Component {
     constructor(props) {
         super(props);
 
-        console.log("Binary Gap");
     }
 
     render() {
         return (
-            <div>Binary Gap</div>
+            <div>
+                <h2>Binary Gap</h2>
+                <div className="row"> 
+                    <div className="col-sm-6">
+                        <div className="col-sm-12">
+                            <label htmlFor="input">Input</label>
+                            <input type="text" id="input" className="form-control" placeholder="Please write a number here" value=""/>    
+                        </div> 
+                        <div className="col-sm-12">
+                            <br/>
+                            <button className="btn btn-info btn-block">Run</button>
+                        </div>
+                    </div> 
+                    <div className="col-sm-6" id="divOutput">
+
+                    </div> 
+                </div>
+            </div>
         );
     }
 }
