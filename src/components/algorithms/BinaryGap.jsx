335462c701446c7d7781ccaebff2cc69f0c2c0fb
Mateo Guzmán <mateo.guzmanalvarez@gmail.com>
2017-08-30 12:35:50 -0500

    Convert number to binary

--- src/components/algorithms/BinaryGap.jsx
+++ src/components/algorithms/BinaryGap.jsx
@@ -4,7 +4,26 @@ export default class BinaryGap extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            inputNumber: ''
+        };
 
+        this.calculateBinaryGap = this.calculateBinaryGap.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({inputNumber: event.target.value});
+    }
+
+    calculateBinaryGap(){
+        let number = this.state.inputNumber;
+        
+        this.printOutput(Number(number).toString(2));
+    }
+
+    printOutput(output){
+        $("#divOutput").html("<h3>"+output+"</h3>");
     }
 
     render() {
@@ -15,11 +34,23 @@ export default class BinaryGap extends Component {
                     <div className="col-sm-6">
                         <div className="col-sm-12">
                             <label htmlFor="input">Input</label>
-                            <input type="text" id="input" className="form-control" placeholder="Please write a number here" value=""/>    
+                            <input 
+                                type="text" 
+                                id="input" 
+                                className="form-control" 
+                                placeholder="Please write a number here" 
+                                value={this.state.inputNumber}
+                                onChange={this.handleChange}
+                            />    
                         </div> 
                         <div className="col-sm-12">
                             <br/>
-                            <button className="btn btn-info btn-block">Run</button>
+                            <button 
+                                className="btn btn-info btn-block" 
+                                onClick={this.calculateBinaryGap}
+                            >
+                                Run
+                            </button>
                         </div>
                     </div> 
                     <div className="col-sm-6" id="divOutput">
