91618247469175566a9dbcfe4afd56c354f3b3ee
Mateo Guzmán <mateo.guzmanalvarez@gmail.com>
2017-10-31 08:07:21 -0500

    Added Binary Gap description.

--- src/components/algorithms/BinaryGap.jsx
+++ src/components/algorithms/BinaryGap.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Description from '../Description';
 
 export default class BinaryGap extends Component {
 
@@ -43,7 +44,29 @@ export default class BinaryGap extends Component {
     render() {
         return (
             <div>
-                <h2>Binary Gap</h2>
+                <Description title={"Binary Gap"}>
+                <h5>Find longest sequence of zeros in binary representation of an integer.</h5>
+                <br/><br/>
+                A binary gap within a positive integer N is any maximal sequence of consecutive zeros that is surrounded by ones at both ends in the binary representation of N.
+                <br/><br/>
+                For example, number 9 has binary representation 1001 and contains a binary gap of length 2. The number 529 has binary representation 1000010001 and contains two binary gaps: one of length 4 and one of length 3. The number 20 has binary representation 10100 and contains one binary gap of length 1. The number 15 has binary representation 1111 and has no binary gaps.
+                <br/><br/>
+                Write a function:
+                <br/><br/>
+                function solution(N);
+                <br/><br/>
+                that, given a positive integer N, returns the length of its longest binary gap. The function should return 0 if N doesn't contain a binary gap.
+                <br/><br/>
+                For example, given N = 1041 the function should return 5, because N has binary representation 10000010001 and so its longest binary gap is of length 5.
+                <br/><br/>
+                Assume that:
+                <br/><br/>
+                N is an integer within the range [1..2,147,483,647].
+                Complexity:
+                <br/><br/>
+                expected worst-case time complexity is O(log(N));
+                expected worst-case space complexity is O(1).
+                </Description>
                 <div className="row"> 
                     <div className="col-sm-6">
                         <div className="col-sm-12">
@@ -52,7 +75,7 @@ export default class BinaryGap extends Component {
                                 type="text" 
                                 id="input" 
                                 className="form-control" 
-                                placeholder="Please write a number here" 
+                                placeholder="Please write a number here, example: 529" 
                                 value={this.state.inputNumber}
                                 onChange={this.handleChange}
                             />    
