aaca4d38b9b1bfcc1e54a96b51378db18b06fc32
Mateo Guzmán <mateo.guzmanalvarez@gmail.com>
2017-11-22 15:34:33 -0500

    connecting mongo database

--- server/core/config/config.dev.js
+++ server/core/config/config.dev.js
@@ -6,7 +6,7 @@ config.logFileDir = path.join(__dirname, '../../log');
 config.logFileName = 'app.log';
 config.dbHost = process.env.dbHost || 'localhost';
 config.dbPort = process.env.dbPort || '27017';
-config.dbName = process.env.dbName || 'trains';
+config.dbName = process.env.dbName || 'algorithms';
 config.serverPort = process.env.serverPort || 3000;
 
 export default config;
