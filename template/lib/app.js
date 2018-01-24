d60d73600a748ffb6f11e9ad826123bb2a6f4b1a
Mateo Guzmán <mateo.guzmanalvarez@gmail.com>
2018-01-24 10:29:45 -0500

    Renamed example folder by public.

--- template/lib/app.js
+++ template/lib/app.js
@@ -34,7 +34,7 @@
 /******/ 	__webpack_require__.c = installedModules;
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/Users/mateoguzmana/Documents/Algorithms/template/example";
+/******/ 	__webpack_require__.p = "/Users/mateoguzmana/Documents/Algorithms/template/public";
 /******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(0);
